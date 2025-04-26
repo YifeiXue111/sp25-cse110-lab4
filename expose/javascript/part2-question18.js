cc610be39a58dbe352d0fc3e1b9992525d858596
YifeiXue111 <156253841+YifeiXue111@users.noreply.github.com>
2025-04-25 19:43:41 -0700

    done

--- expose/javascript/part2-question18.js
+++ expose/javascript/part2-question18.js
@@ -3,3 +3,25 @@ setInterval(() => {
     let time = d.toLocaleTimeString();
     console.log(time);
 }, 1000);
+
+
+
+function printSum() {
+    debugger
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    num1 = parseInt(num1, 10);
+    num2 = parseInt(num2, 10);
+    document.getElementById("sum").innerHTML = "Sum: " + calculateSum(num1, num2);
+  }
+  
+  function calculateSum(num1, num2) {
+    let result = num1 + num2;
+    return result;
+  }
+  
+  function init() {
+    document.getElementById('printSum').addEventListener('click', printSum);
+  }
+  
+  window.addEventListener('DOMContentLoaded', init);
